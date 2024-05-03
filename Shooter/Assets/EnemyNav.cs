ebc8e55baf65e61f375976b7b9ceba775cee957e
Robostroyer123 <148308707+Robostroyer123@users.noreply.github.com>
2024-05-04 07:14:24 +1200

    EnemyAIAdded

--- Shooter/Assets/EnemyNav.cs
+++ Shooter/Assets/EnemyNav.cs
@@ -5,10 +5,22 @@ using UnityEngine.AI;
 
 public class EnemyNav : MonoBehaviour
 {
-    public float chaseRange, stopRange;
-    public float shootRange;
+    public float sightRange, stopRange;
+    public float attackRange;
+    
+    [Space]
+    public float walkPointRange = 5;
+    public float walkPointStop = 1;
+    [Space]
+    public float attackCooldown = 1; 
+    public float suspicionTime = 2;
 
     public Transform gunSet;
+
+    float timeSinceLastAttack, timeSinceLastSeen;
+
+    bool walkPointSet;
+    Vector3 walkPoint;
 
     Hitscan hitscan;
     NavMeshAgent agent;
@@ -16,6 +28,7 @@ public class EnemyNav : MonoBehaviour
     Transform Player { get { return GameObject.FindWithTag("Player").transform; } }
     Vector3 PlayerCentre { get { return Player.TryGetComponent(out Collider col) ? col.bounds.center : Player.position; } }
     float DistanceToPlayer { get { return Vector3.Distance(transform.position, Player.transform.position); } }
+    bool WithinRange(float range) { return DistanceToPlayer < range; }
     // Start is called before the first frame update
     void Start()
     {
@@ -32,37 +45,63 @@ public class EnemyNav : MonoBehaviour
     void Update()
     {
         if (Player == null || (health != null && health.IsDead)) return;
-        if(gunSet != null)
+        timeSinceLastAttack += Time.deltaTime;
+        timeSinceLastSeen += Time.deltaTime;
+        //if(gunSet != null)
+        //{
+        //    gunSet.LookAt(PlayerCentre);
+        //}
+        if (WithinRange(sightRange)) _ = timeSinceLastSeen = 0;
+        if (!WithinRange(sightRange) && !WithinRange(attackRange) && timeSinceLastSeen > suspicionTime) { Patrol(); }
+        if (!WithinRange(sightRange) && !WithinRange(attackRange) && timeSinceLastSeen <= suspicionTime) { Suspect(); }
+        if (WithinRange(sightRange) && !WithinRange(attackRange)) { Chase(); }
+        if (WithinRange(sightRange) && WithinRange(attackRange)) { Attack(); }
+    }
+    void Patrol()
+    {
+        if (!walkPointSet) { SearchWalkPoint(); }
+        else { agent.SetDestination(walkPoint); }
+        
+        if(Vector3.Distance(transform.position, walkPoint) < walkPointStop)
         {
-            gunSet.LookAt(PlayerCentre);
-        }
-        if(hitscan != null)
-        {
-            if(DistanceToPlayer < shootRange)
-            {
-                hitscan.Shoot();
-            }
-        }
-        if(DistanceToPlayer < chaseRange && DistanceToPlayer > stopRange)
-        {
-            agent.isStopped = false;
-            agent.SetDestination(Player.transform.position);
-        }
-        else
-        {
-            agent.isStopped = true;
+            walkPointSet = false;
         }
     }
-
+    void Suspect()
+    {
+        agent.SetDestination(transform.position);
+    }
+    void SearchWalkPoint()
+    {
+        Vector3 randomPoint = transform.position + Random.insideUnitSphere * walkPointRange;
+        if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, 1.0f, NavMesh.AllAreas))
+        {
+            walkPoint = hit.position;
+            walkPointSet = true;
+        }
+    }
+    void Chase()
+    {
+        agent.SetDestination(Player.position);
+    }
+    void Attack()
+    {
+        if(timeSinceLastAttack > attackCooldown)
+        {
+            agent.SetDestination(transform.position);
+            hitscan.Shoot();
+            timeSinceLastAttack = 0;
+        }
+    }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, chaseRange);
+        Gizmos.DrawWireSphere(transform.position, sightRange);
 
-        Gizmos.color = Color.black;
-        Gizmos.DrawWireSphere(transform.position, shootRange);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, walkPointRange);
 
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, stopRange);
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
